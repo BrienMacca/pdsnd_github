82144144ab9e5890ea4857f6a62c15d5a541c634
BrienMacca <bggssmalls@gmail.com>
2022-02-28 21:52:21 +1100

    refactor: add detail to time stat comment
    Additional detail to describe what time stat will provide on line 93 of
    the bikeshare code. This indicates the frequencies that will be provided

--- bikeshare.py
+++ bikeshare.py
@@ -89,7 +89,8 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+    This provides the most common month, day or hour"""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
